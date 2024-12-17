572306083db69668a12aaf977a13655d6ae05acc
rabunthoeun7777 <rabunthoeun7777@gmail.com>
2024-12-17 16:57:25 +0700

    add dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,26 @@
-import Footer from '@/components/footer/footer';
-import MyHeader from '@/components/my-header';
-import MyService from '@/components/my-service';
+import { ThemeProvider } from '@/components/my-theme-provider';
 import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import { MyAreaChartStackedComponent } from "@/components/my-area-chart-stacked"
+import { MyBarChartMultipleComponent } from "@/components/my-bar-chart-multiple"
+import { MyLineChartInteractiveComponent } from "@/components/my-line-chart-interactive"
+import { ModeToggle } from "@/components/my-mode-toggle"
+import { MyPieChartComponent } from "@/components/my-pie-chart"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+
 
 const ClientLayout = (
     {
@@ -14,9 +33,45 @@ const ClientLayout = (
 ) => {
     return (
         <>
-            <main className=''>
-                {children}
-            </main>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <SidebarProvider>
+                    <AppSidebar />
+                    <SidebarInset>
+                        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                            <div className="flex gap-4">
+                                <div className="flex items-center gap-2 px-4">
+                                    <SidebarTrigger className="-ml-1" />
+                                    <Separator orientation="vertical" className="mr-2 h-4" />
+                                    <Breadcrumb>
+                                        <BreadcrumbList>
+                                            <BreadcrumbItem className="hidden md:block">
+                                                <BreadcrumbLink href="#">
+                                                    Building Your Application
+                                                </BreadcrumbLink>
+                                            </BreadcrumbItem>
+                                            <BreadcrumbSeparator className="hidden md:block" />
+                                            <BreadcrumbItem>
+                                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                            </BreadcrumbItem>
+                                        </BreadcrumbList>
+                                    </Breadcrumb>
+                                </div>
+                                <div>
+                                    <ModeToggle />
+                                </div>
+                            </div>
+                        </header>
+                        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                            {children}
+                        </main>
+                    </SidebarInset>
+                </SidebarProvider>
+            </ThemeProvider>
         </>
     )
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,22 @@
-import Products from "./products/page";
+import { MyAreaChartStackedComponent } from '@/components/my-area-chart-stacked'
+import { MyBarChartMultipleComponent } from '@/components/my-bar-chart-multiple'
+import { MyLineChartInteractiveComponent } from '@/components/my-line-chart-interactive'
+import { MyPieChartComponent } from '@/components/my-pie-chart'
+import React from 'react'
 
-export default function Home() {
-  return (
-    <div className="max-w-screen-xl mx-auto">
-      <Products/>
-
-    </div>
-  );
+const page = () => {
+    return (
+        <div>
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" ><MyAreaChartStackedComponent /></div>
+                <div className="aspect-video rounded-xl bg-muted/50" ><MyBarChartMultipleComponent /></div>
+                <div className="aspect-video rounded-xl bg-muted/50" ><MyPieChartComponent /></div>
+            </div>
+            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" >
+                <MyLineChartInteractiveComponent />
+            </div>
+        </div>
+    )
 }
+
+export default page
